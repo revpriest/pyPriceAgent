2c8cade0238e8f30ca5c820be2b90073db66b979
Pre <pre@dalliance.net>
2020-02-11 00:28:08 +0000

    example secrets.py shows we can override tickers file

--- secrets_example.py
+++ secrets_example.py
@@ -4,4 +4,5 @@ API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 Email_Report_Address = ['your@email.com']
 OUT_CSV_FILE = "/home/user/py/pyPriceAgent/latestPrices.csv"
 OUT_HTML_FILE = "/home/user/py/pyPriceAgent/latestPrices.html"
+TickersName = "myTickers.txt"
 

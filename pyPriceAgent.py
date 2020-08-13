e4ab70ed1b6196724f25b243b0d4cffcb4cb0849
Pre <pre@dalliance.net>
2020-08-13 22:29:16 +0100

    Notes on bad data.
    I'm getting bad data from the API I think.
    
    EG, ROBG on the 12th August currently nearly 100x
    lower than it should be.
    
    Tried changing the history-period to see if
    a different cache somewhere would make it
    better but it didn't.
    
    Some of the bad data from the API seemed to get
    repaired as I investigated, but some bad data
    remains.
    
    Hopefully if it's getting back-fixed we should
    overwrite all the bad-data in my own cache
    over time. Extend data-grab to 3 months coz
    it appears to be costless to me, probably is
    nearly costless to them, and hopefully gives
    them longer to fix the bad data if they get
    bad data sometimes and then fix it later.

--- pyPriceAgent.py
+++ pyPriceAgent.py
@@ -585,7 +585,7 @@ def appendLatestPriceDataStocks(ticker,data):
   else:
     fetched = yf.download(
               tickers = tickerGroupString,
-              period =   "1mo",    # 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+              period =   "3mo",    # 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
               interval = "1d",     # 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
               group_by = 'column', # "column", "ticker"
               auto_adjust = True,  # True, False. Maybe include splits etc?
